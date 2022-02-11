888df64d25beb3ec985e2c5045de36098fc3b240
Rômulo Júnio <86726833+Romz16@users.noreply.github.com>
2022-02-11 12:27:01 -0300

    Feat: Interaction between admin and user

---
+++ src/services/UsersService.ts
@@ -0,0 +1,33 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+class UsersService {
+  private usersRespository: Repository<User>;
+
+  constructor() {
+    this.usersRespository = getCustomRepository(UsersRepository);
+  }
+
+  async create(email: string) {
+    const userExists = await this.usersRespository.findOne({ email });
+
+    if (userExists) {
+      return userExists;
+    }
+
+    const user = this.usersRespository.create({ email });
+
+    await this.usersRespository.save(user);
+
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    const user = await this.usersRespository.findOne({ email });
+
+    return user;
+  }
+}
+
+export { UsersService };
